f1e56c8a7c43f99d12d46e19d1fe20a0540082e2
zhoushiqi47 <619482220@qq.com>
2019-01-22 09:39:33 +0800

    add

---
+++ Scattering Coefficient Code/Pear.m
@@ -0,0 +1,27 @@
+function [x,y]=Pear(t,type) 
+%rotate
+ a=sin(pi/4);
+ p=3;
+ d = 0.5;
+ rho = d*(2+0.3*cos(p*t));
+ drho = -d*p*0.3*sin(p*t);
+ ddrho =-d*p^2*0.3*cos(p*t);
+ if type==1
+     x1 = rho.*cos(t);   %% primal function
+     y1 = rho.*sin(t);
+     x = a*x1-a*y1;
+     y = a*x1+a*y1;
+ else if type==2
+         x1 = -rho.*sin(t)+drho.*cos(t);  %% derivative of order one
+         y1 = rho.*cos(t)+sin(t).*drho ;
+         x = a*x1-a*y1;
+         y = a*x1+a*y1;
+     else if type==3     %% derivative of order two
+             x1 = ddrho.*cos(t) - 2*drho.*sin(t) + rho.*cos(t) ;
+             y1 = ddrho.*sin(t) + 2*drho.*cos(t) - rho.*sin(t);
+             x = a*x1-a*y1;
+             y = a*x1+a*y1;
+         end
+     end
+ end
+ return
